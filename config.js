394e3adef09a1bd7eb865cfa3c33211fc990fb4d
Akshat Singh Tomar <akshatsinghtomar06@gmail.com>
2026-08-11 22:43:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://mhbbspjkzqrjoiptkjol.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_SgvcpROQd2ClG9HzsAL3UA_AzqI0zvw";
+const SUPABASE_ANON_KEY = "your-secret-key";
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.supabaseClient = supabaseClient;
